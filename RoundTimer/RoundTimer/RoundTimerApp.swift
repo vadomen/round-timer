8f6d274e0ad72d3fdd00cbecab2304149c2ff299
Vadim Pospelov <vadomen@gmail.com>
2025-12-25 13:59:09 +0200

    refactor: reimplement main ContentView with timer/settings UI, removing CoreData list management and related persistence setup.

--- RoundTimer/RoundTimer/RoundTimerApp.swift
+++ RoundTimer/RoundTimer/RoundTimerApp.swift
@@ -1,21 +1,10 @@
-//
-//  RoundTimerApp.swift
-//  RoundTimer
-//
-//  Created by Vadim Pospelov on 25.12.2025.
-//
-
 import SwiftUI
-import CoreData
 
 @main
 struct RoundTimerApp: App {
-    let persistenceController = PersistenceController.shared
-
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
